58412f5f5fd76e0d0e627689a452d7d5fbe54bf8
Prakash-27 <prakashs23007@gmail.com>
2024-11-15 02:03:33 +0530

    express-sessions part-2, passport.js passport-local  Authentication

--- src/session.mjs
+++ src/session.mjs
@@ -2,6 +2,7 @@ import express, { request, response } from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -28,6 +29,57 @@ app.get("/", (request, response) => {
     request.session.visited = true;
     response.cookie("hello", "world", { maxAge: 30000, signed: true });
     response.status(201).send({ msg: "Hello World!" });
+});
+
+app.post("/api/auth", (request, response) => {
+  const { body: { username, password }, } = request;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if(!findUser || findUser.password !== password) { 
+    return response.status(401).send({ msg: "BAD CREDENTIALS" });
+  }  
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (request, response) => {
+  request.sessionStore.get(request.session.id, (err, sessionData) => {
+    if(err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(sessionData);
+  });
+  return request.session.user 
+    ? response.status(200).send(request.session.user) 
+    : response.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.post("/api/cart", (request, response) => {
+  if(!request.session.user) {
+    return response.sendStatus(401);
+  }
+  const { body: item } = request;
+  const { cart } = request.session;
+  if(!cart) {
+    request.session.cart = [item];
+  } else {
+    cart.push(item);
+  }
+  return response.status(201).send(item);
+});
+
+app.get("/api/cart", (request, response) => {
+  request.sessionStore.get(request.session.id, (err, sessionData) => {
+    if(err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(sessionData);
+  });
+  if(!request.session.user) {
+    return response.sendStatus(401);
+  }
+  return response.send(request.session.cart ?? []);
 });
 
 app.listen(PORT, () => {
